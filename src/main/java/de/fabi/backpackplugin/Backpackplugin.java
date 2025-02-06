a5525f47d1428d3680e686e99c8b50cb89dad93c
Fabii <fabianschhdh@gmail.com>
2025-02-06 17:58:51 +0000

    Changes: IDk

--- src/main/java/de/fabi/backpackplugin/Backpackplugin.java
+++ src/main/java/de/fabi/backpackplugin/Backpackplugin.java
@@ -1,5 +1,13 @@
 package de.fabi.backpackplugin;
+/*
+Copyright © 2024 https://github.com/Fabii08?tab=repositories  
+All rights reserved.  
 
+Unauthorized copying, modification, or distribution of this file,  
+via any medium, is strictly prohibited.  
+
+DO NOT DISTRIBUTE.
+*/
 import de.fabi.backpackplugin.Listeners.BackpackListener;
 import de.fabi.backpackplugin.Listeners.Openbackpack;
 import de.fabi.backpackplugin.commands.BackpackCommand;
--- src/main/java/de/fabi/backpackplugin/Listeners/BackpackListener.java
+++ src/main/java/de/fabi/backpackplugin/Listeners/BackpackListener.java
@@ -1,4 +1,13 @@
 package de.fabi.backpackplugin.Listeners;
+/*
+Copyright © 2024 https://github.com/Fabii08?tab=repositories  
+All rights reserved.  
+
+Unauthorized copying, modification, or distribution of this file,  
+via any medium, is strictly prohibited.  
+
+DO NOT DISTRIBUTE.
+*/
 
 import de.fabi.backpackplugin.logic.BackpackManager;
 import org.bukkit.event.EventHandler;
